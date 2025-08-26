efcd8e201dd304c34e4dac446ea9cab63ca2712f
Work-Satyam1 <worksatyam62@gmail.com>
2025-08-27 01:22:15 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,41 +1,6 @@
-// import amazon from "../assets/amazon.png";
-// import flipkart from "../assets/flipkart.png";
-// import heroImage from "../assets/hero-image.png";
-
-// const Hero = () => {
-//   return (
-//     <div className="hero container">
-//       <div className="hero-content">
-//         <h1>YOUR FEET DESERVE THE BEST</h1>
-//         <p>
-//           Your feet deserve the best and we are here to help you with our shoe.
-//           Your feet deserve the best and we are here to help you with our shoe.
-//         </p>
-
-//         <div className="hero-btn">
-//           <button>Shop Now</button>
-//           <button className="sec-btn">Category</button>
-//         </div>
-
-//         <div className="shopping">
-//           <p>Available only</p>
-//           <div className="brand-icons">
-//             <img src={amazon} alt="Amazon" />
-//             <img src={flipkart} alt="Flipkart" />
-//           </div>
-//         </div>
-//       </div>
-
-//       <div className="hero-image">
-//         <img src={heroImage} alt="Hero" />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-
+import amazon from "../assets/amazon.png";
+import flipkart from "../assets/flipkart.png";
+import heroImage from "../assets/hero-image.png";
 
 const Hero = () => {
   return (
@@ -55,18 +20,21 @@ const Hero = () => {
         <div className="shopping">
           <p>Available only</p>
           <div className="brand-icons">
-            <img src="/vite-project/amazon.png" alt="Amazon" />
-            <img src="/vite-project/flipkart.png" alt="Flipkart" />
+            <img src={amazon} alt="Amazon" />
+            <img src={flipkart} alt="Flipkart" />
           </div>
         </div>
       </div>
 
       <div className="hero-image">
-        <img src="/vite-project/hero-image.png" alt="Hero" />
+        <img src={heroImage} alt="Hero" />
       </div>
     </div>
   );
 };
 
 export default Hero;
+
+
+
 
